c6b9dc4ac959ddbfc0aece12f9e8d7fc0a965b78
ary <ary@clappingape.com>
2020-05-06 13:58:46 +0700

    update asset onboarding

--- app/src/main/java/app/beelabs/com/mopay/ui/activity/OnboardingActivity.kt
+++ app/src/main/java/app/beelabs/com/mopay/ui/activity/OnboardingActivity.kt
@@ -1,13 +1,32 @@
 package app.beelabs.com.mopay.ui.activity
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import app.beelabs.com.mopay.R
+import app.beelabs.com.mopay.ui.adapter.OnBoardingViewPagerAdapter
+import app.beelabs.com.mopay.ui.fragment.OnboardingFirstFragment
+import app.beelabs.com.mopay.ui.fragment.OnboardingSecondFragment
+import app.beelabs.com.mopay.ui.fragment.OnboardingThirdFragment
+import kotlinx.android.synthetic.main.activity_onboarding.*
 
 class OnboardingActivity : AppActivity() {
+
+    private var currentPage = 0
+    private val fragments = ArrayList<Fragment>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
         setupLightStatusBarMode()
+
+        val adapter = OnBoardingViewPagerAdapter(supportFragmentManager)
+
+        fragments.add(OnboardingFirstFragment())
+        fragments.add(OnboardingSecondFragment())
+        fragments.add(OnboardingThirdFragment())
+
+        adapter.addFragments(fragments)
+        viewPager.adapter = adapter
+
     }
 }
