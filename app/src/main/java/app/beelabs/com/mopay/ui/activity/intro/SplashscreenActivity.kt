c2ca89296cd92d61226d39d7e8587dbc81234b91
ary <ary@clappingape.com>
2020-05-07 11:37:30 +0700

    update style screen and flow to onboarding

--- app/src/main/java/app/beelabs/com/mopay/ui/activity/intro/SplashscreenActivity.kt
+++ app/src/main/java/app/beelabs/com/mopay/ui/activity/intro/SplashscreenActivity.kt
@@ -1,13 +1,24 @@
 package app.beelabs.com.mopay.ui.activity.intro
 
+import android.content.Intent
 import android.os.Bundle
+import app.beelabs.com.codebase.support.rx.RxTimer
 import app.beelabs.com.mopay.R
 import app.beelabs.com.mopay.ui.activity.AppActivity
 
 class SplashscreenActivity : AppActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setupLightStatusBarMode()
         setContentView(R.layout.activity_splashscreen)
+
+        intent = Intent(SplashscreenActivity@this, OnboardingActivity::class.java)
+        RxTimer.doTimer(10000, false, object: RxTimer(){
+            override fun onCallback(along: Long?) {
+
+                startActivity(intent)
+                finish()
+
+            }
+        })
     }
 }
