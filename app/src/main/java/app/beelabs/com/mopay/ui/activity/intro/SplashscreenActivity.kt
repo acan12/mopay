7df74c9c7636d73aebceef087c02c04af5965681
ary <ary@clappingape.com>
2020-05-13 17:02:24 +0700

    update style for onboarding and splashscreen

--- app/src/main/java/app/beelabs/com/mopay/ui/activity/intro/SplashscreenActivity.kt
+++ app/src/main/java/app/beelabs/com/mopay/ui/activity/intro/SplashscreenActivity.kt
@@ -5,19 +5,24 @@ import android.os.Bundle
 import app.beelabs.com.codebase.support.rx.RxTimer
 import app.beelabs.com.mopay.R
 import app.beelabs.com.mopay.ui.activity.AppActivity
+import app.beelabs.com.mopay.ui.activity.MainActivity
 
 class SplashscreenActivity : AppActivity() {
+
+    private lateinit var splashIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
 
-        intent = Intent(SplashscreenActivity@this, OnboardingActivity::class.java)
-        RxTimer.doTimer(10000, false, object: RxTimer(){
+        if (true)
+            splashIntent = Intent(SplashscreenActivity@ this, OnboardingActivity::class.java)
+        else
+            splashIntent = Intent(SplashscreenActivity@ this, MainActivity::class.java)
+        RxTimer.doTimer(10000, false, object : RxTimer() {
             override fun onCallback(along: Long?) {
-
-                startActivity(intent)
+                startActivity(splashIntent)
                 finish()
-
             }
         })
     }
