bf162b906d03a7c79e41e22e476a3e5751186860
ary <ary@clappingape.com>
2020-05-21 16:34:49 +0700

    update list adapter for product list in main layout

---
+++ app/src/main/java/app/beelabs/com/mopay/model/pojo/Product.kt
@@ -0,0 +1,4 @@
+package app.beelabs.com.mopay.model.pojo
+
+class Product {
+}
--- app/src/main/java/app/beelabs/com/mopay/ui/activity/MainActivity.kt
+++ app/src/main/java/app/beelabs/com/mopay/ui/activity/MainActivity.kt
@@ -1,9 +1,14 @@
 package app.beelabs.com.mopay.ui.activity
 
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import app.beelabs.com.codebase.base.BaseActivity
 import app.beelabs.com.mopay.R
+import app.beelabs.com.mopay.model.pojo.Product
+import app.beelabs.com.mopay.ui.adapter.ProductAdapter
 import app.beelabs.com.mopay.ui.impl.IMainView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppActivity(), IMainView {
 
@@ -11,6 +16,11 @@ class MainActivity : AppActivity(), IMainView {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupLightStatusBarMode()
+
+        dataProduct.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = ProductAdapter(ArrayList<Product>())
+        }
     }
 }
 
